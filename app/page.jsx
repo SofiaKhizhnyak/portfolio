eae733cae59ea27c183fcbca9214466c13b13ce7
Sofia Khizhnyak <pissboyyz@gmail.com>
2025-07-16 13:03:26 +0300

    fixed delays in page transitions

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ function Home() {
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        transition: { delay: 2.2, duration: 0.5, ease: "easeIn" },
       }}
       className="h-screen flex items-center"
     >
--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -11,7 +11,7 @@ function PageTransition({ children }) {
           initial={{ opacity: 1 }}
           animate={{
             opacity: 0,
-            transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+            transition: { delay: 0.5, duration: 0.2, ease: "easeInOut" },
           }}
           className="min-h-screen w-screen fixed top-0 pointer-events-none"
         ></motion.div>
